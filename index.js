d6e41d0d2dcbb5a034eecb2e6c57a9a7e56e001d
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-11-01 16:03:42 +0530

    frontend url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://your-frontend-domain.com"],
+    origin: ["http://localhost:3000", "https://vance-fullstack.vercel.app/"],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
