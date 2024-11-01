fc77c08c64385cbf588a3e34931c6aaebee715d8
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-11-01 16:27:42 +0530

    cors update

--- index.js
+++ index.js
@@ -5,23 +5,17 @@ import cron from "node-cron";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-};
-
-const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["http://localhost:3000", "https://vance-fullstack.vercel.app"],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
