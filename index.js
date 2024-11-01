a24e0f43f3df6145ac5ed1cffdff39ad68b83d37
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-11-01 13:42:03 +0530

    port env file

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ import cors from "cors";
 import cron from "node-cron";
 import { PrismaClient } from "@prisma/client";
 import chromium from "chrome-aws-lambda";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200,
 };
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
